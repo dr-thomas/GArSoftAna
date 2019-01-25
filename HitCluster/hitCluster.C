7394d16025316170ff813fa6035f681a4fd61bf3
Thomas Campbell <thomascampbell1@gmail.com>
2019-01-25 09:10:03 -0700

    access trak and traj hits

--- HitCluster/hitCluster.C
+++ HitCluster/hitCluster.C
@@ -53,39 +53,61 @@ void hitCluster() {
 		posBins[ii][2] = (posBins[ii][1]-posBins[ii][0])/binSize;
 	}
 
-	TH1F* totalResX = new TH1F("totalResX","",200,-200,200);
-	TH1F* totalResY = new TH1F("totalResY","",200,-200,200);
+	TH1F* totalResX = new TH1F("totalResX","",200,-1,1);
+	TH1F* totalResY = new TH1F("totalResY","",200,-1,1);
 	TH1F* totalResZ = new TH1F("totalResZ","",200,-1,1);
 	for(int iEntry=0; iEntry<inT->GetEntries(); iEntry++){
 		if(iEntry%((inT->GetEntries())/10)==0) cout << iEntry*100./(inT->GetEntries()) << "%" << endl;
 		inT->GetEntry(iEntry);
 		if(gEvt->CCNC->at(0)!=0) continue;
 		if(std::abs(gEvt->PDG->at(0))!=13) continue;
-		cout << "event: " << iEntry << endl;
-		for(uint ii=0; ii<gEvt->TrackHitX->size(); ii++){
-			for(uint jj=0; jj<gEvt->TrackHitX->at(ii).size(); jj++){
-				float hitX = gEvt->TrackHitX->at(ii).at(jj);
-				if(TMath::Abs(hitX)<1e-6) continue;
-				if(TMath::Abs(hitX)>1e9) continue;
-				totalResX->Fill(hitX);
-			}
-			for(uint jj=0; jj<gEvt->TrajHitX->at(ii).size(); jj++){
-				float hitX = gEvt->TrajHitX->at(ii).at(jj);
-				if(TMath::Abs(hitX)<1e-6) continue;
-				if(TMath::Abs(hitX)>1e9) continue;
-				totalResY->Fill(hitX);
-			}
+		for(uint iTrk=0; iTrk<gEvt->TrackHitX->size(); iTrk++){
+			if(gEvt->TrackHitX->at(iTrk).size() < 10) continue;
 
-		}
+			for(uint jTrk=0; jTrk<gEvt->TrackHitX->at(iTrk).size(); jTrk++){
+
+				float hitX = gEvt->TrackHitX->at(iTrk).at(jTrk);
+				float hitY = gEvt->TrackHitY->at(iTrk).at(jTrk);
+				float hitZ = gEvt->TrackHitZ->at(iTrk).at(jTrk);
+
+				float minDiff=555e10;
+				float closeHitTrj[3] = {-555e10,-555e10,-555e10};
+
+				for(uint iTrj=0; iTrj<gEvt->TrajHitX->size(); iTrj++){
+					for(uint jTrj=0; jTrj<gEvt->TrajHitX->at(iTrj).size(); jTrj++){
+						float hitTrjX = gEvt->TrajHitX->at(iTrj).at(jTrj);
+						float hitTrjY = gEvt->TrajHitY->at(iTrj).at(jTrj);
+						float hitTrjZ = gEvt->TrajHitZ->at(iTrj).at(jTrj);
+						float tempDiff = (hitX-hitTrjX)*(hitX-hitTrjX);
+						tempDiff *= (hitY-hitTrjY)*(hitY-hitTrjY);
+						tempDiff *= (hitZ-hitTrjZ)*(hitZ-hitTrjZ);
+						if(tempDiff<minDiff){
+							minDiff=tempDiff;
+							closeHitTrj[0] = hitTrjX;
+							closeHitTrj[1] = hitTrjY;
+							closeHitTrj[2] = hitTrjZ;
+						}
+					}
+				}
+				totalResX->Fill(hitX-closeHitTrj[0]);
+				totalResY->Fill(hitY-closeHitTrj[1]);
+				totalResZ->Fill(hitZ-closeHitTrj[2]);
+			} // hits in tracks
+		} // tracks
 	}
 
 	TCanvas* c = new TCanvas;
 	totalResX->Draw();
-	c->Print("~/Desktop/test.eps");
+	c->Print("~/Desktop/resX.eps");
 
 	c = new TCanvas;
 	totalResY->Draw();
-	c->Print("~/Desktop/test2.eps");
+	c->Print("~/Desktop/resY.eps");
+
+	c = new TCanvas;
+	totalResZ->Draw();
+	c->Print("~/Desktop/resZ.eps");
+
 	 
 
 	inF->Close();
