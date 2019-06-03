818df42c738ed4afdef433b5cd8de503dbcfe269
Thomas Campbell <thomascampbell1@gmail.com>
2019-06-03 12:14:27 -0600

    added some things in tcAna for Susan

--- tcAna.C
+++ tcAna.C
@@ -28,6 +28,12 @@ void tcAna() {
 
 	for(int iEntry=0; iEntry<inT->GetEntries(); iEntry++){
 		inT->GetEntry(iEntry);
+		cout << gEvt->PDG->size() << endl;
+		cout << "PDGS: " << endl;
+		for(int ii=0; (ii<gEvt->PDG->size() && ii<10); ii++){
+			cout << gEvt->PDG->at(ii) << endl;
+		}
+		cout << "done" << endl;
 	}
 	inF->Close();
 }
